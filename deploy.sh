3b6f474b7b259ed41f58e821b2ae8d776f79b3e1
Millaguie <javi@millaguie.net>
2020-08-11 20:56:16 +0200

    propper order

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -e
 
 sudo apt install -y lftp
 
-lftp -c "open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv revientaelplaneta.com . ; quit;"
+lftp -c "open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public/* revientaelplaneta.com/ ; quit;"
