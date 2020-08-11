9cd8bdf9ebca6ebac75ca0117308f88592498eab
Millaguie <javi@millaguie.net>
2020-08-11 20:46:37 +0200

    change to bionic

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-sudo apt-get install -y lftp
+sudo apt install -y lftp
 
 lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public revientaelplaneta.com/; quit;"
