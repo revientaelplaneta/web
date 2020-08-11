d071d3d36490689be7021cae02e685b4a1667f06
Millaguie <javi@millaguie.net>
2020-08-11 21:00:47 +0200

    now tested

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ set -e
 
 sudo apt install -y lftp
 
-lftp -c "open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public/* revientaelplaneta.com/ ; quit;"
+cd public
+lftp -c "open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv . revientaelplaneta.com/ ; quit;"
