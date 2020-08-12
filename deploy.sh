071cbf8f6c167a244b0e8fafa4b8f4d4f0ef1f82
Millaguie <javi@millaguie.net>
2020-08-12 22:52:36 +0200

    change (again) lftp command

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -e
 sudo apt install -y lftp
 
 cd public
-lftp -f "open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv --verbose . revientaelplaneta.com/ ; bye;"
+lftp -c "open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv --verbose . revientaelplaneta.com/ ; bye;"
